72abcd1e4527afd8988296872ccbcca72c8dea9f
vildehalvorsen <vilde_ha96@hotmail.com>
2021-08-29 08:35:39 +0200

    update

---
+++ js/error/errorMessage.js
@@ -0,0 +1,3 @@
+function errorMessage(message = "An error occurred") {
+    return `<div class="error">${message}</div>`;
+}
--- js/question-2.js
+++ js/question-2.js
@@ -21,31 +21,35 @@
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=137eca6b734a49f186ad4151035f7b65";
 
-const resultsContainer = document.querySelector(".results");
+const resultsContainer = document.querySelector(".games");
 
 
 async function getGames() {
-    const response = await fetch(url);
+    try {
+        const response = await fetch(url);
 
-    const data = await response.json();
+        const data = await response.json();
 
-    const results = data.results;
+        const results = data.results;
 
-    resultsContainer.innerHTML = "";
+        resultsContainer.innerHTML = "";
 
-    for (let i = 0; i < results.length; i++) {
-        let names = results[i].name;
-        let ratings = results[i].rating;
-        let tags = results[i].tags.length;
+        for (let i = 0; i < results.length; i++) {
+            let names = results[i].name;
+            let ratings = results[i].rating;
+            let tags = results[i].tags.length;
 
-        if (i === 8) {
-            break;
+            if (i === 8) {
+                break;
+            }
+
+            resultsContainer.innerHTML +=
+                `<div class="gamesinfo"> <h2>Name of game:</h2> ${names}<h2>Rating:</h2> ${ratings} <h2>Amount of tags:</h2> ${tags}</div>`;
         }
-
-        resultsContainer.innerHTML +=
-            `<div class="result"> <h2>Name of game:</h2> ${names}<h2>Rating:</h2> ${ratings} <h2>Amount of tags:</h2> ${tags}</div>`;
+    } catch (error) {
+        console.log("An error occurred")
+        resultsContainer.innerHTML = errorMessage("An error occurred when calling the API");
     }
-
 }
 
 getGames();
